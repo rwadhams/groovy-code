8ff6157219c3d326b452be045fd0cc7bcc3785b2
robert.wadhams <robert.wadhams@gmail.com>
2020-11-29 10:44:31 +1100

    Variable trip planning data lists

--- GroovyStuff/src/main/groovy/com/wadhams/trip/planner/app/TripPlannerApp.groovy
+++ GroovyStuff/src/main/groovy/com/wadhams/trip/planner/app/TripPlannerApp.groovy
@@ -19,14 +19,42 @@ class TripPlannerApp {
 	}
 	
 	def execute() {
+		//trip_01
+//		List<LocationNights> ldlist = planTrip_01()
+//		int year = 2020
+//		int month = 6	//0-11 month values
+//		int day = 12
+//		report(ldlist, year, month, day)
+		
+		//trip_02
+		List<LocationNights> ldlist = planTrip_02()
+		int year = 2020
+		int month = 10	//0-11 month values
+		int day = 28
+		report(ldlist, year, month, day)
+	}
+	
+	def report(List<LocationNights> ldlist, int year, int month, int day) {
 		SimpleDateFormat sdf = new SimpleDateFormat('EEE, MMM d, yyyy')
 		
-		GregorianCalendar startingCal = new GregorianCalendar(2020,6,12)
+		GregorianCalendar startingCal = new GregorianCalendar(year, month, day)
 		Date startingDate = startingCal.time 
 //		println sdf.format(startingDate)
 //		println ''
 		
+		Date reportDate = startingCal.time
+		ldlist.each {ld ->
+			ld.nights.times {
+				println "${sdf.format(reportDate)}\t${ld.location}"
+				reportDate = reportDate.next()
+			}
+			
+		}
+	}
+	
+	List<LocationNights> planTrip_01() {
 		List<LocationNights> ldlist = []
+		
 		ldlist << new LocationNights(location : 'Deception Bay (Housesitting)', nights : 1)
 		ldlist << new LocationNights(location : 'Nanango (RV Park)', nights : 4)
 		ldlist << new LocationNights(location : 'Maidenwell (behind Pub)', nights : 4)
@@ -45,16 +73,24 @@ class TripPlannerApp {
 		ldlist << new LocationNights(location : 'Hervey Bay', nights : 5)
 		ldlist << new LocationNights(location : 'Beerwah', nights : 1)
 		ldlist << new LocationNights(location : 'Car and caravan storage', nights : 1)
-		
-		Date reportDate = startingCal.time
-		ldlist.each {ld ->
-			ld.nights.times {
-				println "${sdf.format(reportDate)}\t${ld.location}"
-				reportDate = reportDate.next()
-			}
-			
-		}
+
+		return ldlist		
 	}
+	
+	List<LocationNights> planTrip_02() {
+		List<LocationNights> ldlist = []
+		
+		ldlist << new LocationNights(location : 'Urunga', nights : 2)
+		ldlist << new LocationNights(location : 'Moonee Beach', nights : 3)
+		ldlist << new LocationNights(location : 'Minnie Waters', nights : 3)
+		ldlist << new LocationNights(location : 'Ballina', nights : 2)
+		ldlist << new LocationNights(location : 'Murwillumbah', nights : 2)
+		ldlist << new LocationNights(location : 'Beenleigh', nights : 1)
+		ldlist << new LocationNights(location : 'Car and caravan storage', nights : 1)
+
+		return ldlist
+	}
+	
 }
 
 @ToString
