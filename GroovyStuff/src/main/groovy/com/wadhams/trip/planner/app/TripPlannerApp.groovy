3e8b248598311c34cb29375ed8d72b5bbb80d0a3
robert.wadhams <robert.wadhams@gmail.com>
2020-11-29 11:42:02 +1100

    refactored TripPlanner app to file data input

--- GroovyStuff/src/main/groovy/com/wadhams/trip/planner/app/TripPlannerApp.groovy
+++ GroovyStuff/src/main/groovy/com/wadhams/trip/planner/app/TripPlannerApp.groovy
@@ -11,38 +11,39 @@ class TripPlannerApp {
 		println 'TripPlannerApp started...'
 		println ''
 
-		TripPlannerApp app = new TripPlannerApp()
-		app.execute()
+		if (args.size() > 0) {
+			//args[0] must be a number (nn = 01, 02, 03...99). Filename pattern will be data/TripPlanner<nn>.xml
+			String fn = "data/TripPlanner${args[0]}.xml"
+			println "Filename: $fn"
+			println ''
+			
+			TripPlannerApp app = new TripPlannerApp()
+			app.execute(fn)
+		}
+		else {
+			println 'Missing parameter(s). Application did not run.'
+		}
 		
 		println ''
 		println 'TripPlannerApp ended.'
 	}
 	
-	def execute() {
-		//trip_01
-//		List<LocationNights> ldlist = planTrip_01()
-//		int year = 2020
-//		int month = 6	//0-11 month values
-//		int day = 12
-//		report(ldlist, year, month, day)
+	def execute(String filename) {
+		def tripPlanner = new XmlSlurper().parse(new File(filename))
 		
-		//trip_02
-		List<LocationNights> ldlist = planTrip_02()
-		int year = 2020
-		int month = 10	//0-11 month values
-		int day = 28
-		report(ldlist, year, month, day)
+		SimpleDateFormat sdf = new SimpleDateFormat('yyyyMMdd')
+		Date startDate = sdf.parse(tripPlanner.startDate.text())
+//		println startDate
+		
+		List<LocationNights> ldlist = buildLocationNightsList(tripPlanner.locationNights)
+		
+		report(startDate, ldlist)
 	}
 	
-	def report(List<LocationNights> ldlist, int year, int month, int day) {
+	def report(Date startingDate, List<LocationNights> ldlist) {
 		SimpleDateFormat sdf = new SimpleDateFormat('EEE, MMM d, yyyy')
 		
-		GregorianCalendar startingCal = new GregorianCalendar(year, month, day)
-		Date startingDate = startingCal.time 
-//		println sdf.format(startingDate)
-//		println ''
-		
-		Date reportDate = startingCal.time
+		Date reportDate = startingDate
 		ldlist.each {ld ->
 			ld.nights.times {
 				println "${sdf.format(reportDate)}\t${ld.location}"
@@ -52,42 +53,15 @@ class TripPlannerApp {
 		}
 	}
 	
-	List<LocationNights> planTrip_01() {
+	List<LocationNights> buildLocationNightsList(locationNights) {
 		List<LocationNights> ldlist = []
 		
-		ldlist << new LocationNights(location : 'Deception Bay (Housesitting)', nights : 1)
-		ldlist << new LocationNights(location : 'Nanango (RV Park)', nights : 4)
-		ldlist << new LocationNights(location : 'Maidenwell (behind Pub)', nights : 4)
-		ldlist << new LocationNights(location : 'Chinchilla (Showgrounds)', nights : 1)
-		ldlist << new LocationNights(location : 'Miles (Ben and Gillian\'s)', nights : 2)
-		ldlist << new LocationNights(location : 'Roma (Gun Club)', nights : 3)
-		ldlist << new LocationNights(location : 'Carnavon Gorge (Sandstone Park)', nights : 5)
-		ldlist << new LocationNights(location : 'Springsure (Lion\'s free camp)', nights : 1)
-		ldlist << new LocationNights(location : 'Sapphire (Blue Gem Tourist Park)', nights : 4)
-		ldlist << new LocationNights(location : 'Gemfield area (???)', nights : 3)
-		ldlist << new LocationNights(location : 'Blackwater', nights : 2)
-		ldlist << new LocationNights(location : 'Bluff (Blackdown Tablelands)', nights : 2)
-		ldlist << new LocationNights(location : 'Rockhampton area', nights : 3)
-		ldlist << new LocationNights(location : 'Agnes Water', nights : 4)
-		ldlist << new LocationNights(location : 'Bargara', nights : 4)
-		ldlist << new LocationNights(location : 'Hervey Bay', nights : 5)
-		ldlist << new LocationNights(location : 'Beerwah', nights : 1)
-		ldlist << new LocationNights(location : 'Car and caravan storage', nights : 1)
-
-		return ldlist		
-	}
-	
-	List<LocationNights> planTrip_02() {
-		List<LocationNights> ldlist = []
+		locationNights.each {ln ->
+			String location = ln.location
+			String nights = ln.nights
+			ldlist << new LocationNights(location : "$location", nights : Integer.parseInt(nights))
+		}
 		
-		ldlist << new LocationNights(location : 'Urunga', nights : 2)
-		ldlist << new LocationNights(location : 'Moonee Beach', nights : 3)
-		ldlist << new LocationNights(location : 'Minnie Waters', nights : 3)
-		ldlist << new LocationNights(location : 'Ballina', nights : 2)
-		ldlist << new LocationNights(location : 'Murwillumbah', nights : 2)
-		ldlist << new LocationNights(location : 'Beenleigh', nights : 1)
-		ldlist << new LocationNights(location : 'Car and caravan storage', nights : 1)
-
 		return ldlist
 	}
 	
