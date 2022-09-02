5a7acf2b41dab76f442c9bb7841575bfff92e3de
robert.wadhams <robert.wadhams@gmail.com>
2022-09-02 14:28:45 +1000

    Holiday App to produce HTML reports

--- GroovyStuff/src/main/groovy/com/wadhams/holidays/app/HolidayApp.groovy
+++ GroovyStuff/src/main/groovy/com/wadhams/holidays/app/HolidayApp.groovy
@@ -42,6 +42,7 @@ class HolidayApp {
 //		println stateList
 //		println ''
 		
+		//txt files
 		File f1 = new File("out/${year}-school-holiday-report.txt")
 		f1.withPrintWriter {pw ->
 			reportTermHolidays(stateList, year, pw)
@@ -55,6 +56,22 @@ class HolidayApp {
 		File f3 = new File("out/${year}-holidays-by-date-report.txt")
 		f3.withPrintWriter {pw ->
 			reportHolidaysByDate(stateList, year, pw)
+		}
+		
+		//html files
+		File f1a = new File("out/${year}-school-holiday-report.html")
+		f1a.withPrintWriter {pw ->
+			reportTermHolidaysHtml(stateList, year, pw)
+		}
+		
+		File f2a = new File("out/${year}-holidays-by-state-report.html")
+		f2a.withPrintWriter {pw ->
+			reportHolidaysByStateHtml(stateList, year, pw)
+		}
+		
+		File f3a = new File("out/${year}-holidays-by-date-report.html")
+		f3a.withPrintWriter {pw ->
+			reportHolidaysByDateHtml(stateList, year, pw)
 		}
 	}
 	
@@ -157,6 +174,106 @@ class HolidayApp {
 			pw.println ''
 		}
 		pw.println ''
+	}
+	
+	def reportHolidaysByStateHtml(List<State> stateList, int year, PrintWriter pw) {
+		DateTimeFormatter dayFormatter = DateTimeFormatter.ofPattern('EEE')
+		DateTimeFormatter ddmmyyyyFormatter = DateTimeFormatter.ofPattern('dd/MM/yyyy')
+		pw.println '<html>'
+		pw.println "<b>Holidays reported by state for $year</b>"
+		pw.println '</br>'
+		stateList.each {s ->
+			pw.println s.name
+			pw.println '<table border="1">'
+			s.holidays.each {h ->
+				pw.println '<tr>'
+				pw.println "<td>${h.date.format(dayFormatter)}</td><td>${h.date.format(ddmmyyyyFormatter)}</td><td>${h.name}</td>"
+				pw.println '</tr>'
+			}
+			pw.println '</table>'
+		}
+		pw.println '</html>'
+	}
+	
+	def reportHolidaysByDateHtml(List<State> stateList, int year, PrintWriter pw) {
+		DateTimeFormatter dayFormatter = DateTimeFormatter.ofPattern('EEE')
+		DateTimeFormatter ddmmyyyyFormatter = DateTimeFormatter.ofPattern('dd/MM/yyyy')
+		pw.println '<html>'
+		pw.println "<b>Holidays reported by date for $year</b>"
+		pw.println '</br>'
+
+		//build map keyed by LocalDate		
+		Map<LocalDate, Map<String, List>> dateMap = [:] as TreeMap
+		stateList.each {s ->
+			s.holidays.each {h ->
+				Map<String, List> holidayMap = dateMap[h.date]
+				if (!holidayMap) {
+					holidayMap = [:] as TreeMap
+					holidayMap[h.name] = [s.name]
+					dateMap[h.date] = holidayMap
+				}
+				else {
+					List<String> holidayStateList = holidayMap[h.name]
+					if (!holidayStateList) {
+						holidayMap[h.name] = [s.name]
+					}
+					else {
+						holidayStateList << s.name
+					} 
+				}
+			}
+		}
+
+		pw.println '<table border="1">'
+		dateMap.each {k1,holidayMap ->
+			holidayMap.each {k2,holidayStateList ->
+				pw.println "<tr><td>${k1.format(dayFormatter)}</td><td>${k1.format(ddmmyyyyFormatter)}</td><td>${k2}</td><td>$holidayStateList</td></tr>"
+			}
+		}
+		pw.println '</table>'
+		pw.println '</html>'
+	}
+	
+	def reportTermHolidaysHtml(List<State> stateList, int year, PrintWriter pw) {
+		DateTimeFormatter ddmm = DateTimeFormatter.ofPattern('dd/MM')
+
+		pw.println '<html>'
+		pw.println "<b>School Holidays for $year</b>"
+		pw.println '</br>'
+		
+		List<String> holidayNameList = ['BeforeSchoolYear','Autumn School Holidays','Winter School Holidays','Spring School Holidays','AfterSchoolYear']
+		holidayNameList.size().times {index ->
+			pw.println holidayNameList[index]
+			pw.println '<table border="1">'
+			Set<LocalDate> termHolidays = []
+			stateList.each {s ->
+				termHolidays.addAll(s.termHolidays[index])
+			}
+			List<LocalDate> termHolidaysSorted = termHolidays.sort()
+	
+			pw.println '<tr align="center">'
+			termHolidaysSorted.each {ld ->
+				pw.print "<td>${ld.format(ddmm)}</td>"
+			}
+			pw.println '</tr>'
+			
+			stateList.each {s ->
+				String stateName = s.name
+				
+				pw.println '<tr align="center">'
+				termHolidays.each {ld ->
+					if (s.termHolidays[index].contains(ld)) {
+						pw.print "<td>${stateName}</td>"
+					}
+					else {
+						pw.print '<td></td>'
+					}
+				}
+				pw.println '</tr>'
+			}
+			pw.println '</table>'
+		}
+		pw.println '</html>'
 	}
 	
 	List<State> buildStateList(states) {
